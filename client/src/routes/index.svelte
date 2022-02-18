3d218925b598cd27dc5eb6e207b07821a2b94561
Joe Carré <carre.joe3@gmail.com>
2022-02-18 10:59:42 +0000

    added a basic card to the front page

--- client/src/routes/index.svelte
+++ client/src/routes/index.svelte
@@ -1,2 +1,16 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="container paper margin-top margin-bottom">
+  <Card
+    title="My awesome Paper card!"
+    image="https://picsum.photos/768"
+  >
+    Card Content
+    <p slot="bottom">
+      <Button>Check it out</Button>
+    </p>
+  </Card>
+</div>
+
+<script>
+  import 'papercss/dist/paper.min.css'
+  import { Button, Card } from 'spaper'
+</script>
