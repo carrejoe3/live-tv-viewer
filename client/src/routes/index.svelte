16e9b57acbfe8030e610936917ca308e3210b74e
Joe Carré <carre.joe3@gmail.com>
2022-02-18 12:54:56 +0000

    added twitch iframe and slight styling

--- client/src/routes/index.svelte
+++ client/src/routes/index.svelte
@@ -1,7 +1,6 @@
 <script>
   import store from '../store';
   import { onMount } from 'svelte';
-	import { Button, Card } from 'spaper';
   import 'papercss/dist/paper.min.css';
 
   onMount(async () => {
@@ -9,11 +8,30 @@
   })
 </script>
 
-<div class="container paper margin-top margin-bottom">
-	<Card title="My awesome Paper card!" image="https://picsum.photos/768">
-		Card Content
-		<p slot="bottom">
-			<Button>Check it out</Button>
-		</p>
-	</Card>
+<div class="container">
+  <div id="iframeContainer" class="paper">
+    <iframe
+      title="Twitch Player"
+      src="https://player.twitch.tv/?channel=twitchpresents&parent=localhost"
+      height={"100%"}
+      width={"100%"}
+      allowfullscreen={true}>
+    </iframe>
+  </div>
 </div>
+
+<style>
+
+  .container {
+    width: 100vw;
+    height: 100vh;    
+    display: flex;
+  }
+
+  #iframeContainer {
+    display: flex;
+    flex-grow: 1;
+    flex-direction: column;
+  }
+
+</style>
---
+++ client/src/services/twitch.ts
@@ -0,0 +1,7 @@
+import TwitchApi from "node-twitch";
+
+export const twitch = new TwitchApi({
+	client_id: "yvqfdqp22phcjy4owdcwa1jd8ksezc",
+	client_secret: "hgfmvfsuqvb4tiur9iqsfrqk2vrbj1"
+});
+
